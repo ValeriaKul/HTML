61d267ef9a437f407fffebc3ffbe1d615a9b4634
ValeriaKul <lera.dribnyuk@mail.ru>
2022-10-14 20:26:03 +0300

    Lesson_2

---
+++ FrontEnd/Lesson_2(03.08.2022)/scripts/script2.js
@@ -0,0 +1,15 @@
+//СВОЙСТВА 
+let string = 'Valeriia Kulikova';
+let symbol = string[0];
+console.log(string.length); // считает длину строки
+console.log(symbol);
+console.log(string[string.length -1]); // вернет последний символ строки
+
+let array = [1, 2, 3, 4, 5];  //5
+console.log(array.length);
+
+//МЕТОДЫ
+
+console.log(string.toLowerCase()); //возвращает строку в нижнем регистре
+console.log(string.toUpperCase()); //возвращает строку в ВЕРХНЕМ регистре
+ocnsole.log(string.trim());        //возвращает строку без пробелов
