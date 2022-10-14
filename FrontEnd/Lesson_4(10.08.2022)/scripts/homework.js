0e1be0c60fe7dce003fdf1b985d8cbb441e2bae0
ValeriaKul <lera.dribnyuk@mail.ru>
2022-10-15 02:05:13 +0300

    Lesson_4

--- FrontEnd/Lesson_4(10.08.2022)/scripts/homework.js
+++ FrontEnd/Lesson_4(10.08.2022)/scripts/homework.js
@@ -2,10 +2,12 @@
 //    Выведите результат в консоль. 
 // console.log('Task_1:');
 function getTextLength (text) {
-    let textLength = text.length;
-    return textLength;
+    return text.length;
 }
+
+let getTextLength_arrow = (text) => text.length;
 // console.log(getTextLength('Hello world'));
+// console.log(getTextLength_arrow('Hello world'));
 
 // 2. Напишите функцию, которая принимает два аргумента (основание степени и саму степень) и 
 //    возвращает число в указанной степени. Значение степени должно быть указано по умолчанию 2. 
@@ -13,28 +15,35 @@ function getTextLength (text) {
 function getExponentiation (number1, number2 = 2) {
     return Math.pow(number1, number2);
 }
+let getExponentiation_arrow = (number1, number2 = 2) => Math.pow(number1, number2);
 // console.log(getExponentiation(2));
-
+// console.log(getExponentiation_arrow(3));
 // 3. Напишите функцию, которая принимает два аргумента с числовым значением и возвращает большее значение. 
 //    Выведите результат в консоль. Предложите два варианта решения (через условный оператор и Math.max()).
 // console.log('Task_3:');
 function getMax (number1, number2) {
     return Math.max (number1, number2);
 }
+let getMax_arrow = (number1, number2) => Math.max (number1, number2);
 // console.log (getMax(4, 12));
+// console.log (getMax_arrow(4, 12));
 
 function getMaxIf (number1, number2) {
-    let max;
     if (number1 > number2) {
-        max = number1;
-    } else if (number1 < number2) {
-        max = number2;
+       return number1;
     } else {
-        max = number1;
+        return number2;
     }
-    return max;
+}
+let getMaxIf_arrow = (number1, number2) => {
+    if (number1 > number2) {
+        return number1;
+     } else {
+         return number2;
+     }
 }
 // console.log(getMaxIf(5,4));
+// console.log(getMaxIf_arrow(5,4));
 
 // 4. Напишите функцию, принимающую в качестве аргументов три числа и выводящую в консоль отсортированные 
 //    числа по убыванию. Используйте условный оператор.
@@ -78,10 +87,35 @@ function getMin (number1, number2, number3) {
     }
     return `MAX = ${max}\nMID = ${mid}\nMIN = ${min}`;
 }
+let getMin_arrow = (number1, number2, number3) => {
+    if (number1 >= number2 && number1 >= number3) {
+        if (number2 >= number3) {
+            console.log(number1, number2, number3);
+        } else {
+            console.log(number1, number3, number2);
+        }
+    } else if (number2 >= number1 && number2 >= number3) {
+        if (number1 >= number3) {
+            console.log(number2, number1, number3);
+        } else {
+            console.log(number2, number3, number2);
+        }
+    } else {
+        if (number1 >= number2) {
+            console.log(number3, number1, number2);
+        } else {
+            console.log(number3, number2, number1)
+        }
+    }
+}
 // console.log(getMin(5, 8, 2));
 // console.log(getMin(8, 5, 2));
 // console.log(getMin(5, 5, 5));
 // console.log(getMin(5, 2, 5));
+// console.log(getMin_arrow(5, 8, 2));
+// console.log(getMin_arrow(8, 5, 2));
+// console.log(getMin_arrow(5, 5, 5));
+// console.log(getMin_arrow(5, 2, 5));
 
 // 5. Напишите функцию, принимающую в качестве аргументов пять чисел и выводящую в консоль наибольшее из них. 
 //    Используйте условный оператор.
@@ -105,7 +139,28 @@ function getMax2 (number1, number2, number3, number4, number5) {
     }
     return max;
 }
-// console.log('MAX is ' + getMax2 (1, 5, 8, 2, 0));
-// console.log('MAX is ' + getMax2 (5, 5, 8, 2, 0));
-// console.log('MAX is ' + getMax2 (5, 5, 5, 5, 5));
-// console.log('MAX is ' + getMax2 (54, 66, 12, 8, 8));
+
+let getMax2_arrow = (number1, number2, number3, number4, number5) => {
+    let max = number1;
+    if (number2 > number1) {
+        max = number2;
+    }
+    if (max < number3) {
+        max = number3;
+    }
+    if (max < number4) {
+        max = number4;
+    }
+    if (max < number5) {
+        max = number5;
+    }
+    return max;
+}
+console.log('MAX is ' + getMax2 (1, 5, 8, 2, 0));
+console.log('MAX is ' + getMax2 (5, 5, 8, 2, 0));
+console.log('MAX is ' + getMax2 (5, 5, 5, 5, 5));
+console.log('MAX is ' + getMax2 (54, 66, 12, 8, 8));
+console.log('MAX is ' + getMax2_arrow (1, 5, 8, 2, 0));
+console.log('MAX is ' + getMax2_arrow (5, 5, 8, 2, 0));
+console.log('MAX is ' + getMax2_arrow (5, 5, 5, 5, 5));
+console.log('MAX is ' + getMax2_arrow (54, 66, 12, 8, 8));
