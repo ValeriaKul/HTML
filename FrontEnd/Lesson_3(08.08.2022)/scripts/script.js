6f0901a7b37ac5dbbb7b6be526c6ab8dac95279b
ValeriaKul <lera.dribnyuk@mail.ru>
2022-10-14 23:09:22 +0300

    Lesson_3(08.08.2022)

---
+++ FrontEnd/Lesson_3(08.08.2022)/scripts/script.js
@@ -0,0 +1,24 @@
+/* let firstName = prompt('What is your name?');
+let lastName = prompt('What is your last name?');
+const age = prompt('What is your age?')
+
+if (age < 18) {
+console.log(`Hi, ${firstName} ${lastName}. Your age is ${age}`);
+} else if (age > 18) {
+    console.log(`Hello, ${firstName} ${lastName}.`);
+} else {
+    console.log(`Dear, ${firstName} ${lastName}. Your age is ${age}`);
+} */
+
+/* let result = +Math.random().toFixed(1);
+
+if (result > 0.5) {
+    console.log(result);
+    console.log('Win!');
+} else if (result < 0.5) {
+    console.log(result);
+    console.log('Lose!');
+} else {
+    console.log(result);
+    console.log('Draw!')
+}; */
