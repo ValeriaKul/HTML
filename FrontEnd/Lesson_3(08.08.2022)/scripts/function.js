d54c2df6f3a763eb4e187f29c14bf52b87cffc53
ValeriaKul <lera.dribnyuk@mail.ru>
2022-10-14 23:09:32 +0300

    Lesson_3

---
+++ FrontEnd/Lesson_3(08.08.2022)/scripts/function.js
@@ -0,0 +1,32 @@
+/* function getSum (num1, num2) {
+    return num1 + num2
+}
+
+console.log(getSum(1, 2));
+console.log(getSum(5, 5));
+console.log(getSum(5, 2));
+
+function getSum2 (num1, num2 = 3) {
+    return num1 + num2;
+}
+console.log(getSum2(1, 2));
+console.log(getSum2(5));
+
+function getGreeting (firstName = 'Elena') {
+    return `Hello, ${firstName}`;
+}
+console.log(getGreeting('Olga'));
+console.log(getGreeting());
+
+function getMultilply (num1, num2, num3 = 1) {
+    let result = num1 * num2 * num3
+    return result;
+}
+console.log(getMultilply(1, 2)); */
+
+function getNameAndAge (firstName = 'Elena', age = 18) {
+return `Hello, ${firstName}. Your age is ${age}.`;
+}
+console.log (getNameAndAge());
+console.log (getNameAndAge('Anton', 22));
+console.log (getNameAndAge('Anna'));
